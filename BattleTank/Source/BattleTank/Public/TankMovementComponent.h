0f14b788037babc4107990353345af2477ecc108
DEMEISEN\jpc <jpc@parallelthoughts.org>
2017-01-10 09:21:20 +0000

    Changed collision system again to avoid "micro divots"; tank now moves OKish; added movement controller.

---
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+/**
+ * Navigation component allowing fly-by-wire concoltr of the tank by player or AI.
+ */
+UCLASS()
+class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+};
